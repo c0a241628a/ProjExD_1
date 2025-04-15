5863bea3e84a188699cfb7148b11f213d1c2dbae
kaworu <c0a241628a@edu.teu.ac.jp>
2025-04-15 15:57:06 +0900

    7

--- pygame_image.py
+++ pygame_image.py
@@ -9,10 +9,10 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #1
     
-    KK_img = pg.image.load("fig/3.png")
-    KK_img = pg.transform.flip(KK_img,True,False)
+    KK_img = pg.image.load("fig/3.png") #2
+    KK_img = pg.transform.flip(KK_img,True,False) #2
     
     tmr = 0
     while True:
@@ -20,13 +20,14 @@ def main():
             if event.type == pg.QUIT: return
         
         x=tmr
-        screen.blit(bg_img, [-x, 0])
-    
-        screen.blit(KK_img, [300, 200])
+        screen.blit(bg_img, [-x, 0]) #3,6
+        screen.blit(bg_img, [-x+1600,0]) #7
+        
+        screen.blit(KK_img, [300, 200]) #4
         
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200) #5
 
 
 if __name__ == "__main__":
